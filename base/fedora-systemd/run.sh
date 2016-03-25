6d6283c014f61c1a285b4af4a1ee07e6634d69c4
Cadey Dodrill <me@christine.website>
2016-03-24 22:36:21 -0700

    docker systemd

--- base/fedora-systemd/run.sh
+++ base/fedora-systemd/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run --stop-signal=37 -ti -v /sys/fs/cgroup:/sys/fs/cgroup -v /tmp/$(mktemp -d):/run fedora-systemd /sbin/init
+docker run --stop-signal=37 -ti -v /sys/fs/cgroup:/sys/fs/cgroup -v /tmp/$(mktemp -d):/run docker.io/xena/fedora-systemd
