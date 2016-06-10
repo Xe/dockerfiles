fd422006c7d13f398489d307ea70f3d249191bbf
Cadey Dodrill <me@christine.website>
2016-06-10 12:44:06 -0700

    update things

--- base/fedora-systemd/run.sh
+++ base/fedora-systemd/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run --stop-signal=37 -ti -v /sys/fs/cgroup:/sys/fs/cgroup -v /tmp/$(mktemp -d):/run docker.io/xena/fedora-systemd
+docker run --stop-signal=37 -ti -v /etc/machine-id:/etc/host-machine-id:ro -v /sys/fs/cgroup:/sys/fs/cgroup -v /tmp/$(mktemp -d):/run docker.io/xena/fedora-systemd
