3ed56b236bc2abdadd45f5998e6c46cea1f7f311
Christine Dodrill <me@christine.website>
2017-03-12 12:36:18 -0700

    add xeserv packages

--- base/alpine/box.rb
+++ base/alpine/box.rb
@@ -10,7 +10,7 @@ run "apk add --no-cache --virtual xe-alpine-base tini ca-certificates runit libc
 # if libtinfo.so.5 is used, install ncurses5-libs via apk
 run "ln -s /usr/lib/libncurses.so.5 /usr/lib/libtinfo.so.5"
 
-env "BACKPLANE_AGENT_VERSION" => "1.2.3"
+env "BACKPLANE_AGENT_VERSION" => "1.4.0"
 run %q[ apk add -U --no-cache wget ca-certificates \
      && wget https://bin.equinox.io/c/jWahGASjoRq/backplane-stable-linux-amd64.tgz \
      && tar xf backplane-stable-linux-amd64.tgz \
@@ -18,10 +18,10 @@ run %q[ apk add -U --no-cache wget ca-certificates \
      && rm backplane-stable-linux-amd64.tgz \
      && apk del wget ]
 
-# Add glue
 run %q[ apk add -U --no-cache wget \
-     && cd /usr/bin && wget https://xena.greedo.xeserv.us/files/glue \
-     && chmod a+x /usr/bin/glue \
+     && wget https://xena.greedo.xeserv.us/pkg/alpine/edge/core/x86_64/xeserv-keys-1.3-r0.apk \
+     && apk add --allow-untrusted ./xeserv-keys-1.3-r0.apk \
+     && rm ./xeserv-keys-1.3-r0.apk \
      && apk del wget ]
 
 copy "./entrypoint.sh", "/usr/sbin/entrypoint.sh"
