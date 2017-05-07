1fa131a962b2318752f1196f788f03a6afde885a
Christine Dodrill <me@christine.website>
2017-05-07 17:51:27 +0000

    alpine: upgrade xeserv-keys

--- base/alpine/box.rb
+++ base/alpine/box.rb
@@ -11,9 +11,9 @@ run "apk add --no-cache --virtual xe-alpine-base tini ca-certificates runit libc
 run "ln -s /usr/lib/libncurses.so.5 /usr/lib/libtinfo.so.5"
 
 run %q[ apk add -U --no-cache wget \
-     && wget https://xena.greedo.xeserv.us/pkg/alpine/edge/core/x86_64/xeserv-keys-1.3-r0.apk \
-     && apk add --allow-untrusted ./xeserv-keys-1.3-r0.apk \
-     && rm ./xeserv-keys-1.3-r0.apk \
+     && wget https://xena.greedo.xeserv.us/pkg/alpine/edge/core/x86_64/xeserv-keys-1.4-r0.apk \
+     && apk add --allow-untrusted ./xeserv-keys-1.4-r0.apk \
+     && rm ./xeserv-keys-1.4-r0.apk \
      && apk del wget ]
 
 copy "./entrypoint.sh", "/usr/sbin/entrypoint.sh"
