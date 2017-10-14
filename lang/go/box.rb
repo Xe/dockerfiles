9f68b5b3858af7488074501ba00d177354100f53
Christine Dodrill <me@christine.website>
2017-10-14 11:46:07 -0700

    lang/go: go 1.9.1

--- lang/go/box.rb
+++ lang/go/box.rb
@@ -1,10 +1,10 @@
-$goVer = "1.9"
+$goVer = "1.9.1"
 $goVerFam = "1.9"
 
 from "xena/alpine"
 
 run %q[ apk add --no-cache wget && cd /usr/local \
-     && wget -O go.tgz https://storage.googleapis.com/golang/go1.9.linux-amd64.tar.gz \
+     && wget -O go.tgz https://storage.googleapis.com/golang/go1.9.1.linux-amd64.tar.gz \
      && tar xf go.tgz && rm go.tgz ]
 
 env "PATH" => "$PATH:/usr/local/go/bin"
