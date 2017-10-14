909f4b1583c58b5cdcd48ce81b155d2d3f4fd8ee
Christine Dodrill <me@christine.website>
2017-10-14 11:45:41 -0700

    lang/go-mini: go 1.9.1

--- lang/go-mini/box.rb
+++ lang/go-mini/box.rb
@@ -1,8 +1,8 @@
 from "xena/alpine"
 
-run "apk add --no-cache go1.9"
+run "apk add --no-cache go1.9.1"
 copy "./go", "/usr/bin/go"
 
 flatten
-tag "xena/go-mini:1.9"
+tag "xena/go-mini:1.9.1"
 tag "xena/go-mini:1.9"
