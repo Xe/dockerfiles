906977fe5c5cd038de69970e82533ab70d31b747
Christine Dodrill <me@christine.website>
2017-11-02 03:54:44 +0000

    base/alpine: use apk -X

--- base/alpine/box.rb
+++ base/alpine/box.rb
@@ -1,20 +1,14 @@
 from "alpine:edge"
 
-copy "repositories", "/etc/apk/repositories"
 copy "runit/", "/etc/system"
 copy "bin/", "/bin"
 
 run "apk upgrade --no-cache"
 run "apk add --no-cache --virtual xe-alpine-base tini ca-certificates runit libc6-compat"
+run "apk --no-cache -X https://xena.greedo.xeserv.us/pkg/alpine/edge/core/ --allow-untrusted add xeserv-repo"
 
 # if libtinfo.so.5 is used, install ncurses5-libs via apk
 run "ln -s /usr/lib/libncurses.so.5 /usr/lib/libtinfo.so.5"
-
-run %q[ apk add -U --no-cache wget \
-     && wget https://xena.greedo.xeserv.us/pkg/alpine/edge/core/x86_64/xeserv-keys-1.4-r0.apk \
-     && apk add --allow-untrusted ./xeserv-keys-1.4-r0.apk \
-     && rm ./xeserv-keys-1.4-r0.apk \
-     && apk del wget ]
 
 copy "./entrypoint.sh", "/usr/sbin/entrypoint.sh"
 entrypoint "/usr/sbin/entrypoint.sh"
