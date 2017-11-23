25818f853c5870a7d9bd47c4d1c662a55713a420
Christine Dodrill <me@christine.website>
2017-11-22 22:36:13 -0800

    add adelie chroot maker script

---
+++ base/adelie/create_adelie_image.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+set +e
+set +x
+
+version=1.0-alpha3
+
+mkdir adelie
+cd adelie
+
+../apk -X https://distfiles.adelielinux.org/adelie/${version}/system -X https://distfiles.adelielinux.org/adelie/${version}/user -U --allow-untrusted --root . --initdb add adelie-base bash-binsh ssmtp
+
+tar cf ../adelie-$version.tar *
+
+cd ..
+rm -rf adelie
+
+docker import adelie-$version.tar xena/adelie:$version
+docker push xena/adelie:$version
